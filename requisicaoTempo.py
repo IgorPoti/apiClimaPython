3e609db2db277e8f8fab6b347463ac9116b1dbff
IgorPoti <joaooliveira.oliveira134@gmail.com>
2022-12-28 21:22:25 -0300

    Feature: adição de biblioteca para criação de arquivo excel.

--- requisicaoTempo.py
+++ requisicaoTempo.py
@@ -1,4 +1,6 @@
 import requests
+import openpyxl
+from datetime import date
 
 chave = "e92cef9a5f587783fc36b00f48b9aa77"
 
@@ -7,6 +9,33 @@ endereco = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={c
 
 requisicao = requests.get(endereco)
 
+def criarplanilha(cidade, temperatura_minima, temperatura_maxima, clima_atual):
+    data_atual = date.today()
+    # Cria um novo arquivo do Excel
+    workbook = openpyxl.Workbook()
+
+    # Seleciona a primeira aba (Sheet1)
+    worksheet = workbook.active
+
+    # Escreve alguns dados na célula A1
+    worksheet['A1'] = 'Dados do Python'
+
+    # Escreve mais alguns dados nas células abaixo
+    worksheet['A2'] = 'Temperatura Mínima'
+    worksheet['A3'] = 'Temperatura Máxima'
+    worksheet['A4'] = 'Clima atual'
+    worksheet['A5'] = 'Cidade'
+    
+    worksheet['B2'] = temperatura_minima
+    worksheet['B3'] = temperatura_maxima
+    worksheet['B4'] = clima_atual
+    worksheet['B5'] = cidade
+
+    # Salva o arquivo
+    workbook.save(f'dados{cidade}_{data_atual}.xlsx')
+    
+
+
 if requisicao.status_code == 200:
     data = requisicao.json()
     clima_atual = data['weather'][0]['description']
@@ -14,12 +43,13 @@ if requisicao.status_code == 200:
     temperatura_maxima = round(data['main']['temp_max'])
     cidade_selecionada = data['name']
      
+    criarplanilha(cidade_selecionada, temperatura_minima, temperatura_maxima, clima_atual)
     print('========================')
     print('     TEMPERATURA')
     print('========================')
     print(f'Mínima: {temperatura_minima}\nMáxima: {temperatura_maxima}\nO clima está: {clima_atual}\nCidade: {cidade_selecionada}')
 
 else:
-    print('Houve um erro na requisição!')
-    
+    data = requisicao.json()
+    print(f'Erro código: ' + str(data['cod']) + ', entre em contato com o administrador!')
     
